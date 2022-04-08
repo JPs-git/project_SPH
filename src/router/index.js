4d6b55baa94f83cfe63985b3af4f030b31533b92
jieping <2495413242@qq.com>
2022-04-08 22:17:31 +0800

    重写了push和replace，修复了重复匹配相同路由报错的bug

--- src/router/index.js
+++ src/router/index.js
@@ -8,17 +8,25 @@ import Home from '@/views/Home'
 import Login from '@/views/Login'
 import Register from '@/views/Register'
 import Search from '@/views/Search'
-// 备份原本的push方法
-// let origonPush = VueRouter.prototype.push
-// // 重写push、replace方法
-// VueRouter.prototype.push = function(location, resolve, reject){
-//     if(resolve && reject){
-//         origonPush.call(this, location, resolve, reject)
-//     }else{
-//         origonPush.call(this, () => {},() => {})
-//     }
-
-// }
+// 备份原本的push、replace方法
+let originPush = VueRouter.prototype.push
+let originReplace = VueRouter.prototype.replace
+// 重写push、replace方法 以解决重复点击报错
+// 通过重写两个方法，为其添加resolve和reject函数解决
+VueRouter.prototype.push = function(location, resolve, reject){
+    if(resolve && reject){
+        originPush.call(this, location, resolve, reject)
+    }else{
+        originPush.call(this,location, () =>{}, () =>{})
+    }
+}
+VueRouter.prototype.replace = function(location, resolve, reject){
+    if(resolve && reject){
+        originReplace.call(this, location, resolve, reject)
+    }else{
+        originReplace.call(this,location, () =>{}, () =>{})
+    }
+}
 // 新建并暴露路由器
 export default new VueRouter({
     // 配置路由
